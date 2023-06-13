f3c8388ea33ad408a8cfa0bff00cb43407ad7310
dev2 <ko5642027@naver.com>
2023-06-13 23:14:47 +0900

    관심사의 분리

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.config.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -11,8 +12,13 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+//        MemberService memberService = new MemberServiceImpl(memberRepository);
+//        OrderService orderService = new OrderServiceImpl(discountPolicy);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
