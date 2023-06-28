ac00001ac24e675f79036b7be406c5fca52bde49
dev2 <ko5642027@naver.com>
2023-06-29 00:18:04 +0900

    싱글톤 패턴 - private으로 new 키워드를 막고 static으로 실행될때 객체 생성이 이뤄진다. 하나의 인스턴스를 공유할수 있다. - 하지만 코드가 길어지고 유연성이 떨어지는 단점이 발생

--- src/test/java/hello/core/singleton/SingletoneTest.java
+++ src/test/java/hello/core/singleton/SingletoneTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class SingletoneTest {
 
     @Test
@@ -23,5 +25,17 @@ public class SingletoneTest {
 
         Assertions.assertThat(memberService).isNotSameAs(memberService1);
 
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
     }
 }
