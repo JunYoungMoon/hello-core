30a1ddf97dd64f48db53eba908a30b1b52741dba
dev2 <ko5642027@naver.com>
2023-07-02 23:23:15 +0900

    스프링에서 AppConfig를 상속받는 프록시 클래스인 CGLIB$$AppConfig를 생성하고 재정의된 메서드를 통해서 빈을 생성하거나 재활용 한다.
    CGLIB$$AppConfig는 예시 코드이기 때문에 동작하지는 않음 대략 이런식으로 돌아갈것이다 라는 추측

---
+++ src/main/java/hello/core/config/CGLIB$$AppConfig.java
@@ -0,0 +1,26 @@
+package hello.core.config;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import org.springframework.beans.factory.config.SingletonBeanRegistry;
+
+public class CGLIB$$AppConfig extends AppConfig {
+    private final SingletonBeanRegistry singletonBeanRegistry;
+
+    public CGLIB$$AppConfig(SingletonBeanRegistry singletonBeanRegistry) {
+        this.singletonBeanRegistry = singletonBeanRegistry;
+    }
+
+    @Override
+    public MemberService memberService(){
+        //getSingleton으로 빈이름을 키로 존재하는지 여부를 체크한다.
+        //Supplier방식으로 존재 하지 않는다면 AppConfig로 새로운 빈을 등록한다.
+        return (MemberService) this.singletonBeanRegistry.getSingleton("memberService", () -> super.memberService());
+    }
+
+    @Override
+    public MemberRepository memberRepository() {
+        return (MemberRepository) this.singletonBeanRegistry.getSingleton("memberRepository", () -> super.memberRepository());
+    }
+}
+
